c0682896df66425735f8c42b730d2776dae9cb1f
Desmond <daworam@gmail.com>
2024-03-29 13:18:56 -0500

    Initial Commit

---
+++ src/Form.jsx
@@ -0,0 +1,14 @@
+const Form = () => {
+  return (
+    <>
+      <form>
+        <label type="text" name="username">User Name:</label>
+        <input />
+        <label type="password" name="password">Password:</label>
+        <input />
+      </form>
+    </>
+  )
+}
+
+export default Form
